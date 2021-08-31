70c5daa033ff55538a4e460294be0ae768d1777d
Day Yang <maiev@Days-MacBook-Pro.local>
2021-08-30 23:39:28 -0400

    One and done

---
+++ isItUp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+RESPONSE=403
+SLEEP_TIME="60"
+EMAIL="test@gmail.com"
+
+while [ $RESPONSE -eq 403 ] 
+do
+    sleep ${SLEEP_TIME}
+    RESPONSE=$(curl -s -o /dev/null -w "%{http_code}" ${1})
+    echo $RESPONSE
+done
+
+echo -ne '\007' '\007' '\007' # Make a beep sound three times
+echo "$1" | mail -s "It is online." "$EMAIL"
+echo "It is up\!"
